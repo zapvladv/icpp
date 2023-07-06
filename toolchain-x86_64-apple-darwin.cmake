59afdf70cbfdd9bb1686f032c9ef847a81aec8e3
zapvladv <138812631+zapvladv@users.noreply.github.com>
2023-07-06 18:19:00 +0000

    test

---
+++ toolchain-x86_64-apple-darwin.cmake
@@ -0,0 +1,6 @@
+# toolchain-x86_64-apple-darwin.cmake
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER x86_64-apple-darwin-gcc)
+set(CMAKE_CXX_COMPILER x86_64-apple-darwin-g++)
---
+++ toolchain-x86_64-linux-gnu.cmake
@@ -0,0 +1,6 @@
+# toolchain-x86_64-linux-gnu.cmake
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER x86_64-linux-gnu-g++)
